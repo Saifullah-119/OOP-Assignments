504fc235b37e688675c6c702950200bdef737f4a
Saifullah-119 <104373970+Saifullah-119@users.noreply.github.com>
2022-05-09 20:35:14 +0500

    Add files via upload
    Screenshot 2022-05-09 203322 shows error in Scope

---
+++ EscSeq.java
@@ -0,0 +1,7 @@
+class EscSeq
+{
+	public static void main(String args[])
+	{
+	System.out.println("\"1:Once upon a time, there lived a liar whose name was \'rail\'. He liked mangoes\\apples.\b, and disliked pears.\n    Despite his well-built lies, nobody believed him.\r2:\fHe was very interested in characters like \t \u00f2 \t and \t \43 \t.\"" );
+	}
+}
